b0317a25b3c993b42d440efc0d053e8d22f1f78a
shubh <shubh@DESKTOP-6NB74S3>
2019-03-22 10:15:54 +0530

    initial servlet program

---
+++ FirstWeb/src/p1/FirstServlet.java
@@ -0,0 +1,23 @@
+package p1;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+ @WebServlet("/fs")
+public class FirstServlet extends HttpServlet {
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out=response.getWriter();
+		out.println("this is my first servlet");
+	}
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
